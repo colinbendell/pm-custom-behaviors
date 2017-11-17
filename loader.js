a4190767ad43220e80fabb6084e92b87df7c2cb7
Colin Bendell <colin@bendell.ca>
2017-11-16 23:39:04 -0500

    add support deprecation

--- loader.js
+++ loader.js
@@ -10,10 +10,12 @@ function getCustomBehaviorSrc(name, path, description = "") {
 	}});
 }
 
-function createCustomBehavior(name, path) {
+function createCustomBehavior(name, path, isCustomOverride = false) {
 	return getCustomBehaviorSrc(name, path)
         .then(behaviorSrc => {
-            return fetch(`https://control.akamai.com/papi/v0/custom-behaviors`,
+            let url = `https://control.akamai.com/papi/v0/custom-behaviors`;
+            if (isCustomOverride) url = `https://control.akamai.com/papi/v0/custom-overrides`;
+            return fetch(url,
             {
                 method: 'POST',
                 credentials: "same-origin",
@@ -24,8 +26,23 @@ function createCustomBehavior(name, path) {
     	});
 }
 
-function filterExistingBehaviors(behaviors) {
-    return fetch(`https://control.akamai.com/papi/v0/custom-behaviors`, {credentials: "same-origin"})
+function deleteCustomBehavior(behavior, isCustomOverride = false) {
+    let url = `https://control.akamai.com/papi/v0/custom-behaviors/${behavior.behaviorId}`;
+    if (isCustomOverride) url = `https://control.akamai.com/papi/v0/custom-overrides/${behavior.behaviorId}`;
+
+    return fetch(url,
+        {
+            method: 'DELETE',
+            credentials: "same-origin",
+            headers: new Headers({"Content-Type": "application/json",
+                "PAPI-Use-Prefixes": "false"})
+        })
+}
+
+function excludeExistingBehaviors(behaviors, isCustomOverride = false) {
+    let url = `https://control.akamai.com/papi/v0/custom-behaviors`;
+    if (isCustomOverride) url = `https://control.akamai.com/papi/v0/custom-overrides`;
+    return fetch(url, {credentials: "same-origin"})
         .then(response => response.json())
         .then(data => {
             for (const existingBehavior of data.customBehaviors.items) {
@@ -35,8 +52,26 @@ function filterExistingBehaviors(behaviors) {
         })
 }
 
-function main() {
-    return fetch(`https://api.github.com/repos/colinbendell/pm-custom-behaviors/contents/`)
+function matchExistingBehaviors(behaviors, isCustomOverride = false) {
+    let url = `https://control.akamai.com/papi/v0/custom-behaviors`;
+    if (isCustomOverride) url = `https://control.akamai.com/papi/v0/custom-overrides`;
+    return fetch(url, {credentials: "same-origin"})
+        .then(response => response.json())
+        .then(data => {
+            let existingBehaviors = [];
+            for (const existingBehavior of data.customBehaviors.items) {
+                if (behaviors.has(existingBehavior.name))
+                    existingBehaviors.push(existingBehavior);
+            }
+            return existingBehaviors;
+        })
+}
+
+function createAllCustomBehaviors(isCustomOverride = false) {
+    let url = `https://api.github.com/repos/colinbendell/pm-custom-behaviors/contents`;
+    if (isCustomOverride) url = `https://api.github.com/repos/colinbendell/pm-custom-behaviors/contents/overrides`;
+
+    return fetch(url)
         .then(response => response.json())
         .then(data => {
             let behaviorList = new Map();
@@ -46,8 +81,34 @@ function main() {
             });
             return behaviorList;
         })
-        .then(behaviors => filterExistingBehaviors(behaviors))
-        .then(behaviors => Promise.all([...behaviors].map(val => {createCustomBehavior(val[0], val[1])})));
+        .then(behaviors => excludeExistingBehaviors(behaviors, isCustomOverride))
+        .then(behaviors => Promise.all([...behaviors].map(val => {createCustomBehavior(val[0], val[1], isCustomOverride)})));
+}
+
+function deleteAllCustomBehaviors(isCustomOverride = false) {
+    let url = `https://api.github.com/repos/colinbendell/pm-custom-behaviors/contents/deprecated`;
+    if (isCustomOverride) url = `https://api.github.com/repos/colinbendell/pm-custom-behaviors/contents/overrides/deprecated`;
+
+    return fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            let behaviorSet = new Set();
+
+            data.forEach(behaviorFile => {
+                if (/\.xml$/.test(behaviorFile.name) && behaviorFile.size > 0)
+                    behaviorSet.set(behaviorFile.name.replace('.xml', ''), behaviorFile.download_url)
+            });
+            return behaviorSet;
+        })
+        .then(behaviors => matchExistingBehaviors(behaviors, isCustomOverride))
+        .then(behaviors => Promise.all(behaviors.map(o => {deleteCustomBehavior(o, isCustomOverride)})));
+}
+
+function main() {
+    createAllCustomBehaviors();
+    deleteAllCustomBehaviors();
+    createAllCustomOverrides(true);
+    deleteAllCustomBehaviors(true);
 }
 
 main();
